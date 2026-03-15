22b9050e64f0f510785623108bcfcf65de92ffa8
khushi maurya <154513939+surbbhhiii@users.noreply.github.com>
2026-03-15 22:25:38 +0530

    Delete Chapter 5 - Parsing Request directory

--- Chapter 5 - Parsing Request/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const requestHandler = require('./user');
-
-const server = http.createServer(requestHandler);
-
-const PORT = 3001;
-server.listen(PORT, () => {
-  console.log(`Server is running on address http://localhost:${PORT}`);
-});
