888635cb0075e5d1fc092ac33484082c1e83c44a
khushi maurya <154513939+surbbhhiii@users.noreply.github.com>
2026-03-27 15:39:11 +0530

    Create app.js

---
+++ chapter 14 - dynamic path part 2/app.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+
+const storeRouter = require("./routes/storeRouter");
+const hostRouter = require("./routes/hostRouter");
+const rootDir = require("./utils/pathUtil");
+const errorsController = require("./controllers/errors");
+
+const app = express();
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Static
+app.use(express.static(path.join(rootDir, 'public')));
+
+// ✅ ROUTES 
+app.use("/host", hostRouter);
+app.use("/", storeRouter);   
+
+
+
+// 404
+app.use(errorsController.pageNotFound);
+
+// Server
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
